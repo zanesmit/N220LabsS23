0fe2875d23b55519fa0e5db1c720d2e724e2727f
zanesmit <szane200@gmail.com>
2023-01-12 11:40:36 -0500

    Creating lab 0

---
+++ labs/lab0/js/app.js
@@ -0,0 +1,15 @@
+function setup(){
+    createCanvas(1200, 768);
+
+    background(252, 186, 3);
+
+    fill(135, 64, 207);
+
+    rect(40, 50, 100, 20);
+
+
+}
+
+function draw(){
+    circle(mouseX, mouseY, 20);
+}
